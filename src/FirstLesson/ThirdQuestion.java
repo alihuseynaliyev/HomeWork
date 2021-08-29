3c903d06ee857af2626a63bd8e09b2eab6e36a60
Ali Huseynaliyev <alihuseyneliyev12@gmail.com>
2021-08-29 16:50:37 +0400

    Update ThirdQuestion.java

--- src/FirstLesson/ThirdQuestion.java
+++ src/FirstLesson/ThirdQuestion.java
@@ -1,8 +1,44 @@
 package FirstLesson;
 
+import java.util.Scanner;
 
 public class ThirdQuestion {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+//        calculator with switch
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter first number: ");
+        double firstNumber = sc.nextDouble();
+        System.out.println("First number: " + firstNumber);
+
+        System.out.println("Please enter second number: ");
+        double secondNumber = sc.nextDouble();
+        System.out.println("Second number: " + secondNumber);
+
+        System.out.println("Pleae enter calculation number: ");
+        System.out.println("/ 1-Addition / 2-Subtraction /  3-Multiplication / 4-Division /");
+        int calculationNumber = sc.nextInt();
+
+        switch (calculationNumber) {
+            case 1:
+                System.out.println(firstNumber + secondNumber);
+                break;
+
+            case 2:
+                System.out.println(firstNumber - secondNumber);
+                break;
+
+            case 3:
+                System.out.println(firstNumber * secondNumber);
+                break;
+
+            case 4:
+                System.out.println(firstNumber / secondNumber);
+                break;
+            default:
+                System.out.println("Please enter numbers 1 to 4");
+                break;
+        }
     }
 }

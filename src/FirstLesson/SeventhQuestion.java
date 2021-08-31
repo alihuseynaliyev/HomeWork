77cb1ac5b1f559569e0d9da5bab895c2512cb078
Ali Huseynaliyev <alihuseyneliyev12@gmail.com>
2021-09-01 01:02:53 +0400

    Update SeventhQuestion.java

--- src/FirstLesson/SeventhQuestion.java
+++ src/FirstLesson/SeventhQuestion.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 * */
 public class SeventhQuestion {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter Circle Radius");

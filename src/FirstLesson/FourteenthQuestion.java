08797af142449678068194709b9a3e3f8ae7c9c9
Ali Huseynaliyev <alihuseyneliyev12@gmail.com>
2021-09-01 21:38:08 +0400

    Update FourteenthQuestion.java

--- src/FirstLesson/FourteenthQuestion.java
+++ src/FirstLesson/FourteenthQuestion.java
@@ -3,18 +3,18 @@ package FirstLesson;
 import java.util.Scanner;
 
 public class FourteenthQuestion {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter second");
         int seconds = sc.nextInt();
 
         int minutes = seconds / 60;
-        int hours = minutes/60;
-        int days = hours/24;
+        int hours = minutes / 60;
+        int days = hours / 24;
 
 
-        System.out.println(days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds" );
+        System.out.println(days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds");
     }
 }
